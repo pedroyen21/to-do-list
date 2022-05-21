ecc894467dc3050395d3ff32a8fa0af496743e22
Your Name <you@example.com>
2022-05-21 11:04:21 -0300

    first

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+from django.utils.timezone import now
+# Create your models here.
+
+class Task(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name        = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    pub_date    = models.DateField(default=now)
+    conc_date   = models.DateField()
+    concluded   = models.BooleanField(default=False)
